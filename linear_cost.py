19c697fbc16ee3641b9eb0f5901105f7f4a74ee7
ram16230 <ram16230@uvg.edu.gt>
2019-04-02 18:36:54 -0600

    tercer

--- linear_cost.py
+++ linear_cost.py
@@ -11,6 +11,5 @@ def linear_cost(theta, X, y):
 def linear_cost_derivate(theta, X, y):
     m, n = X.shape()
     h = np.matmul(X, theta)
-    sq = (y - h)
-    s = sq * theta
+    s = np.matmul(X.transpose(), (y - h)) # estX.transpose()o no tiene que ser theta
     return s.sum() / m
